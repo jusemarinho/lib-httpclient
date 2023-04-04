688a0e61486aa80f13c6166f1cfffd5d56a5b383
José Paulo <jose.silva@bandtec.com.br>
2023-04-03 23:37:26 -0300

    adicionando método default na interface para mostrar como json

--- src/main/java/br/com/josemarinho/http/HttpResponse.java
+++ src/main/java/br/com/josemarinho/http/HttpResponse.java
@@ -1,5 +1,11 @@
 package br.com.josemarinho.http;
 
+import com.google.gson.Gson;
+
 public interface HttpResponse {
     HttpResponse handle(HttpRequest httpRequest, java.net.http.HttpResponse<?> httpResponseMessage);
+    default String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
 }
