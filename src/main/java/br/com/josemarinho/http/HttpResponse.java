c47ac82b4c999f9ccae8db539357ca638b9e66aa
José Marinho <jose.silva@bandtec.com.br>
2024-12-25 18:14:52 -0300

    feat: removendo método toJson()

--- src/main/java/br/com/josemarinho/http/HttpResponse.java
+++ src/main/java/br/com/josemarinho/http/HttpResponse.java
@@ -1,11 +1,5 @@
 package br.com.josemarinho.http;
 
-import com.google.gson.Gson;
-
 public interface HttpResponse {
     HttpResponse handle(HttpRequest httpRequest, java.net.http.HttpResponse<?> httpResponseMessage);
-    default String toJson() {
-        Gson gson = new Gson();
-        return gson.toJson(this);
-    }
 }
