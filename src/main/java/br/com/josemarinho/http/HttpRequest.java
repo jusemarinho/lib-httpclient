07f4ca77d7197efaa81bbded2ef8525e28b48c64
José Marinho <jose.silva@bandtec.com.br>
2024-12-25 18:14:34 -0300

    chore: removendo getters/setters/contructors para adicionar anotações lombok; incluindo builder para inicialização da mesma

--- src/main/java/br/com/josemarinho/http/HttpRequest.java
+++ src/main/java/br/com/josemarinho/http/HttpRequest.java
@@ -1,11 +1,17 @@
 package br.com.josemarinho.http;
 
+import lombok.*;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 
 import java.awt.*;
 import java.util.Map;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HttpRequest {
 
     private String uriRelative;
@@ -15,62 +21,8 @@ public class HttpRequest {
     private HttpMethod httpMethod;
     private long timeout;
 
-    public HttpRequest(String uriRelative, Map<String, MediaType> stringMediaTypeMap, String payload, HttpMethod httpMethod, Map<String, String> headers, long timeout) {
-        this.uriRelative = uriRelative;
-        this.stringMediaTypeMap = stringMediaTypeMap;
-        this.payload = payload;
-        this.httpMethod = httpMethod;
-        this.headers = headers;
-        this.timeout = timeout;
-    }
-
-    public HttpRequest() { }
-
-    public String getUriRelative() {
-        return uriRelative;
-    }
-
-    public void setUriRelative(String uriRelative) {
-        this.uriRelative = uriRelative;
-    }
-
-    public Map<String, MediaType> getStringMediaTypeMap() {
-        return stringMediaTypeMap;
-    }
-
-    public void setStringMediaTypeMap(Map<String, MediaType> stringMediaTypeMap) {
-        this.stringMediaTypeMap = stringMediaTypeMap;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public String getPayload() {
-        return payload;
-    }
-
-    public void setPayload(String payload) {
-        this.payload = payload;
-    }
-
-    public HttpMethod getHttpMethod() {
-        return httpMethod;
-    }
-
-    public void setHttpMethod(HttpMethod httpMethod) {
-        this.httpMethod = httpMethod;
-    }
-
     public long getTimeout() {
-        return timeout;
+        return timeout == 0 ? 5_000 : timeout;
     }
 
-    public void setTimeout(int timeout) {
-        this.timeout = timeout;
-    }
 }
